46858d5ded7e3177a8762ff76eb12ca043c01dda
ctot7g <hammadbinasad6@gmail.com>
2026-07-17 18:32:42 +0500

    Update layout.tsx
    Added Meta Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,28 @@ export default function RootLayout({
         <meta name="format-detection" content="telephone=no" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
+        <Script id="meta-pixel" strategy="afterInteractive">
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2511714169307241');
+    fbq('track', 'PageView');
+  `}
+</Script>
+        <noscript>
+  <img
+    height="1"
+    width="1"
+    style={{ display: "none" }}
+    src="https://www.facebook.com/tr?id=2511714169307241&ev=PageView&noscript=1"
+  />
+</noscript>
       </head>
       <body
         className={`${openSans.variable} ${bebasNeue.variable} ${openSans.className}`}
